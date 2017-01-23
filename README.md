1376d61a838670ed6fd7e8f532e8f7f47712ab62
Bengt Kleberg <bengt.kleberg@comhem.se>
2017-01-23 21:37:34 +0100

    Modern documentation
    Use EDoc and -spec/-type to allow generation of better documentation.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-erlang_string_io
-================
+Allow io module to read/write an Erlang string.
 
-Make a string usable for io module.
+string_io makes it possible to open/2 an Erlang string, like
+file:open/2 for a file. The pid() returned can be passed to the io
+library module, for reading and writing into the string.
+
+Useful when some module expects to read/write to a file, but you
+rather not create a temporary file for this. The other module must, of
+course, have an interface that handles an io pid(). As luck would have
+it, the one place where I needed this feature, epp_dodger:parse/1, has
+such an interface.
--- src/string_io.erl
+++ src/string_io.erl
@@ -1,23 +1,25 @@
-%% =====================================================================
-%% Allow io operations on an Erlang string
-%%
-%% Copyright (C) 2005 Bengt Kleberg
-%%
-%% This library is free software; you can redistribute it and/or modify
-%% it under the terms of the BSD License.
-%%
-%% You should have received a copy of the BSD
-%% License along with this library; if not, contact the author.
-%%
-%% Author contact: bengt.kleberg@ericsson.com
-%%
-%% ===============================================================%%
-%% @doc Allow io operations on an Erlang string.
-%%
-%%%		<p>This module makes it possible to open an Erlang
-%%%		string. The pid() returned can be passed to the io
-%%%		library module <code>io</code>, for reading and
-%%%		writing into the string.</p>
+%%% @copyright (C) 2017 Bengt Kleberg
+%%%
+%%% This library is free software; you can redistribute it and/or modify
+%%% it under the terms of the BSD License.
+%%%
+%%% You should have received a copy of the BSD
+%%% License along with this library; if not, contact the author.
+%%%
+%%% @author Bengt Kleberg <cean.ebengt@gmail.com>
+%%%
+%%% @doc Allow io operations on an Erlang string.
+%%%		<p>This module makes it possible to open/2 an Erlang
+%%%		string, like file:open/2 for a file.
+%%%		The pid() returned can be passed to the <code>io</code>
+%%%		library module, for reading and/or writing into the string.</p>
+%%%		<p>Useful when some module expects to read/write to a file,
+%%%		but you rather not create a temporary file.
+%%%		The other module must, of course, have an interface that handles an <code>io</code> pid().
+%%%		As luck would have it, the one place where I needed this feature, epp_dodger:parse/1,
+%%%		has such an interface.</p>
+%%% @see epp_dodger
+%%% @end
 
 -module( string_io ).
 
@@ -26,8 +28,26 @@
 	open/2
 ] ).
 
+-type modes()::[read | write | append].
+-type mode()::'append' | 'error' | 'read' | 'read_append' | 'read_write' | 'write'.
 
-%% @spec close(Server::pid()) -> string()
+-record( string_buffer, {
+	is_readonly=false::boolean(), % is it possible to write?
+	is_writeonly=false::boolean(), % is it possible to read?
+	is_append=false::boolean(), % should all writes go to the end of the string?
+	remaining_length=0::integer(), % length of initial string that remains to be read/written
+	remaining=eof::eof | string(), % part of initial string that remains to be read/written
+		% eof is used by get_until()
+	accumulator=[]::string() % part of initial string that has been read/written
+		% it can no longer change
+} ).
+
+%% @doc Close the string IO pid().
+%% @returns the string that has been written to/read from.
+%% Might be useful when writing to.
+%% @throws {error, Description}
+%% @end
+-spec close(Server::pid()) -> string().
 close( Server ) ->
 	Self = erlang:self(),
 	send_to( Server, {stop, Self} ),
@@ -37,16 +57,13 @@ close( Server ) ->
 	end.
 
 
-%% @spec open(String::string(), Modes::list()) -> tuple()
-% input
-%	Modes : list() of atom(). atoms are => read, write, append
-% returns
-%	{ok, pid()}
-% execption
-open( String, Modes )
-		when is_list( String ) ->
+%% @doc Open a string for IO module operations.
+%% The return pid() only works as long as the Erlang process that opened the string is alive.
+%% @end
+-spec open(String::string(), Modes::modes()) -> {ok, pid()} | {error, modes}.
+open( String, Modes ) when is_list( String ) ->
 	case decode( Modes ) of
-	error -> erlang:error( {mode, Modes} );
+	error -> {error, modes};
 	Mode ->
 		Caller = erlang:self(),
 		Fun = fun ( ) ->
@@ -59,14 +76,13 @@ open( String, Modes )
 		{ok, erlang:spawn( Fun )}
 	end.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% input
-%	Modes : list() of atom(). read, write, append are the atoms of interest.
-% returns
-%	read | write | append | read_write | read_append | error
-%
-%	check with string_buffer/2 if any return values are changed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Internal functions
+
+
+% check with string_buffer/2 if any return value is changed
+-spec decode(modes()) -> mode().
 decode( Modes ) ->
 	case {lists:member( read, Modes ),
 			lists:member( write, Modes ),
@@ -80,14 +96,12 @@ decode( Modes ) ->
 	end.
 
 
-% input
-%	Is_readable : boolean(), is the string buffer readable?
-%	Length : integer(), how many chars in String_buffer
-%	How_many : integer(), how many chars that should be read
+% handle io reqest of type get_chars
+%	Is_readable : is the string buffer readable?
+%	Length : how many chars in String_buffer
+%	How_many : how many chars that should be read
 %	String_buffer : #string_buffer{}
-% return
-%	{Read, #string_buffer{}}
-%		Read : string() | eof | {error, {mode, is_writeonly}}
+-spec get_chars(boolean(),integer(),integer(),#string_buffer{}) -> {Result,#string_buffer{}} when Result::'eof' | string() | {'error',{'mode','is_writeonly'}}.
 get_chars( false, _Length, _How_many, String_buffer ) -> {{error, {mode, is_writeonly}}, String_buffer};
 get_chars( true, 0, _How_many, String_buffer ) -> {eof, String_buffer};
 get_chars( true, Length, How_many, String_buffer ) when (Length > How_many) ->
@@ -98,7 +112,8 @@ get_chars( true, _Length, _How_many, String_buffer ) ->
 	{Remaining, string_buffer_after_read( String_buffer, all_read )}.
 
 
-% input
+% handle io reqest of type get_until
+%	Is_readable : is the string buffer readable?
 %	Module : module
 %	Function : function
 %	Arguments : arguments
@@ -107,9 +122,7 @@ get_chars( true, _Length, _How_many, String_buffer ) ->
 %		that Module:Function() wants to have again,
 %		to be able to build the whole result
 %	String_buffer : #string_buffer{}
-% return
-%	{Result, #string_buffer{}}
-%		Result : ok | {error, Reason}
+-spec get_until(boolean(),atom(),atom(),[any()], any(),#string_buffer{}) -> {Result,#string_buffer{}} when Result::'eof' | string() | {'error',{'mode','is_writeonly'}}| {'error',{'apply',any()}}.
 get_until( false, _Module, _Function, _Arguments, _Cont, String_buffer ) ->
 	{{error, {mode, is_writeonly}}, String_buffer};
 get_until( true, Module, Function, Arguments, Cont, String_buffer ) ->
@@ -131,9 +144,8 @@ get_until( _Other, MFA, String_buffer ) ->
 % input
 %	Request : tuple()
 %	Acc : {Status, #string_buffer{}}
-% return
-%	{Result, #string_buffer{}}
-%		Result : ok | {error, Reason}
+% return {Result, #string_buffer{}}
+%	Result : ok | {error, Reason}
 io_request( {put_chars, Chars}, {ok, String_buffer} ) ->
 	put_chars( string_buffer_is_writeable(String_buffer), Chars, String_buffer );
 io_request( {put_chars, unicode, Chars}, {ok, String_buffer} ) ->
@@ -176,8 +188,7 @@ loop( String_buffer ) ->
 
 % handle io reqest of type put chars
 % transform binary to list in input
-% return tuple
-%	{Result, #string_buffer{}}
+-spec put_chars(boolean(),_,#string_buffer{}) -> {'ok' | {'error',_},#string_buffer{}}.
 put_chars( false, _Binary, String_buffer ) ->{{error, {mode, is_readonly}}, String_buffer};
 put_chars( true, Binary, String_buffer ) when is_binary(Binary) ->
 	put_chars( true, erlang:binary_to_list(Binary), String_buffer );
@@ -200,23 +211,9 @@ send_to( Receiver, Data ) ->
 	Receiver ! Data.
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--record( string_buffer, {
-	is_readonly=false, % is it possible to write?
-	is_writeonly=false, % is it possible to read?
-	is_append=false, % should all writes go to the end of the string?
-	remaining_length=0, % length of initial string that remains to be read/written
-	remaining=eof, % part of initial string that remains to be read/written
-		% eof is used by get_until()
-	accumulator=[] % part of initial string that has been read/written
-		% it can no longer change
-} ).
-
-
 % create new #string_buffer{} with String value
 % depending upon Mode the value goes in various places
-% return
-%	#string_buffer{}
+-spec string_buffer(string(),mode()) -> #string_buffer{}.
 string_buffer( String, Mode )->
 	case Mode of
 	read ->
@@ -238,9 +235,8 @@ string_buffer( String, Mode )->
 
 
 % do the things neccessary to modify string buffer after a _read of Chars
-% return
-%	#string_buffer{}
 % first all chars read
+-spec string_buffer_after_read(#string_buffer{},'all_read' | non_neg_integer() | string()) -> #string_buffer{}.
 string_buffer_after_read( String_buffer, all_read ) ->
 	#string_buffer{remaining=Remaining} = String_buffer,
 	string_buffer_modify( String_buffer, Remaining );
@@ -263,8 +259,7 @@ string_buffer_after_read( String_buffer, New_remaining ) ->
 
 
 % do the things neccessary to modify string buffer after a write of Chars
-% return
-%	#string_buffer{}
+-spec string_buffer_after_write(#string_buffer{},string()) -> #string_buffer{}.
 string_buffer_after_write( String_buffer, Chars ) ->
 	#string_buffer{is_append=Append, remaining_length=Remaining_length} =
 		String_buffer,
@@ -294,26 +289,19 @@ string_buffer_after_write( String_buffer, Chars ) ->
 	end.
 
 
-% check if write is ok in string buffer
-% return
-%	true | false
-string_buffer_is_writeable( String_buffer ) ->
-	#string_buffer{is_readonly=Readonly} = String_buffer,
+-spec string_buffer_is_writeable(#string_buffer{}) -> boolean().
+string_buffer_is_writeable( #string_buffer{is_readonly=Readonly} ) ->
 	not Readonly.
 
-% check if read is ok from string buffer
-% return
-%	true | false
-string_buffer_is_readable( String_buffer ) ->
-	#string_buffer{is_writeonly=Writeonly} = String_buffer,
+-spec string_buffer_is_readable(#string_buffer{}) -> boolean().
+string_buffer_is_readable( #string_buffer{is_writeonly=Writeonly} ) ->
 	not Writeonly.
 
 
 % this is a simpler interface to string_buffer_modify/4
 % Done is the new addition to the accumulator
 % if Done is eof we sanitise input
-% return
-%	#string_buffer{}
+-spec string_buffer_modify(#string_buffer{},'eof' | string()) -> #string_buffer{}.
 string_buffer_modify( String_buffer, eof ) ->
 	string_buffer_modify( String_buffer, [], eof, 0 );
 string_buffer_modify( String_buffer, Done ) ->
@@ -323,8 +311,7 @@ string_buffer_modify( String_buffer, Done ) ->
 % Done is the new addition to the accumulator
 % if Remaining is [] we sanitise input
 % if Done is [] we skip append/2
-% return
-%	#string_buffer{}
+-spec string_buffer_modify(#string_buffer{},string(),'eof' | string(),integer()) -> #string_buffer{}.
 string_buffer_modify( String_buffer, Done, [], _Remaining_length ) ->
 	string_buffer_modify( String_buffer, Done, eof, 0 );
 string_buffer_modify( String_buffer, [], Remaining, Remaining_length ) ->
@@ -340,27 +327,23 @@ string_buffer_modify( String_buffer, Done, Remaining, Remaining_length ) ->
 
 
 % get remaining part from string_buffer.
-% return
-%	string() | eof
+-spec string_buffer_remaining(#string_buffer{}) -> 'eof' | string().
 string_buffer_remaining( #string_buffer{remaining=String} ) ->
 	String.
 
 % get How_many characters of remaining part from string_buffer.
-% return
-%	string()
+-spec string_buffer_remaining(#string_buffer{},non_neg_integer()) -> string().
 string_buffer_remaining( #string_buffer{remaining=String}, How_many ) ->
 	lists:sublist( String, How_many ).
 
 % get length of remaining part from string_buffer.
-% return
-%	integer()
+-spec string_buffer_remaining_length(#string_buffer{}) -> integer().
 string_buffer_remaining_length( #string_buffer{remaining_length=Length} ) ->
 	Length.
 
 
 % get all strings from string_buffer as one string.
-% return
-%	string()
+-spec string_buffer_to_string(#string_buffer{}) -> string().
 string_buffer_to_string( #string_buffer{accumulator=A, remaining=R} ) ->
 	Remaining = case R of
 		eof -> [];
